4b4bb032e3292764c2b69405076c8b9f8444636e
Spark Fountain <sparkfountain@yahoo.de>
2020-10-15 23:53:20 +0200

    added alias game routes

--- apps/alias-game-backend/src/app/app.controller.ts
+++ apps/alias-game-backend/src/app/app.controller.ts
@@ -1,13 +1,70 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import {
+  ActiveSession,
+  JoinSession,
+  Response,
+  User,
+} from '@spark-fountain/alias-game';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // TODO: remove this router later again
   @Get()
-  getData() {
+  getData(): { message: string } {
     return this.appService.getData();
   }
+
+  @Post()
+  createSession(@Body() session: ActiveSession): Response<ActiveSession> {
+    // TODO: implement
+    return null;
+  }
+
+  @Get()
+  getSessions(): Response<JoinSession[]> {
+    // TODO: implement
+    return null;
+  }
+
+  @Post()
+  joinSession(@Body() session: ActiveSession): Response<ActiveSession> {
+    // TODO: implement
+    return null;
+  }
+
+  @Post()
+  requestActivePlayer(@Body() user: User): Response<void> {
+    // TODO: implement
+    return null;
+  }
+
+  @Post()
+  resetSession(@Body() sessionName: string): Response<void> {
+    // TODO: implement
+    return null;
+  }
+
+  @Post()
+  startSession(@Body() sessionName: string): Response<void> {
+    // TODO: implement
+    return null;
+  }
+
+  @Post()
+  leaveSession(@Body() sessionName: string): Response<void> {
+    // TODO: implement
+    return null;
+  }
+
+  @Post()
+  nextRound(@Body() sessionName: string): Response<void> {
+    // TODO: implement
+    return null;
+  }
+
+  // TODO: what about "fetch-session"?
 }
