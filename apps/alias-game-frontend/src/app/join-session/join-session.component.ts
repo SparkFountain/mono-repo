ad9421f5b3fc3d14041c8df63f11587dfba41528
Spark Fountain <sparkfountain@yahoo.de>
2020-11-02 00:29:48 +0100

    refactored join session implementation

--- apps/alias-game-frontend/src/app/join-session/join-session.component.ts
+++ apps/alias-game-frontend/src/app/join-session/join-session.component.ts
@@ -11,6 +11,7 @@ import { environment } from '../../environments/environment';
 import { Response } from '@spark-fountain/alias-game';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'spark-fountain-join-session',
@@ -18,11 +19,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./join-session.component.scss'],
 })
 export class JoinSessionComponent implements OnInit {
+  public sessionForm: FormGroup;
+
   public participant: string;
 
   public selectedSession: any;
   public sessions2Join: JoinSession[];
-  public selectedSession2Join: JoinSession;
 
   public selectedTeam2Join: string;
   public teamMembers: string;
@@ -38,11 +40,88 @@ export class JoinSessionComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.sessions2Join = [];
-    this.selectedSession2Join = null;
+    this.sessionForm = new FormGroup({
+      participant: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(16),
+      ]),
+      session: new FormControl('', [Validators.required]),
+      team: new FormControl('', [Validators.required]),
+    });
 
-    this.participant = 'Mr(s). Anonymous';
+    this.sessions2Join = [
+      {
+        name: 'Friday Fun',
+        creator: 'Spark Fountain',
+        teams: [
+          {
+            name: 'Rot',
+            active: false,
+            color: '',
+            players: [],
+            remainingCards: -1,
+          },
+          {
+            name: 'Blau',
+            active: false,
+            color: '',
+            players: [],
+            remainingCards: -1,
+          },
+        ],
+      },
+      {
+        name: 'Corona fahr zur Hölle',
+        creator: 'Gott',
+        teams: [
+          {
+            name: 'Grün',
+            active: false,
+            color: '',
+            players: [],
+            remainingCards: -1,
+          },
+          {
+            name: 'Gelb',
+            active: false,
+            color: '',
+            players: [],
+            remainingCards: -1,
+          },
+        ],
+      },
+    ];
+
+    this.participant = '';
     this.selectedTeam2Join = '';
+  }
+
+  unsetTeam(): void {
+    this.sessionForm.get('team').setValue('');
+  }
+
+  getErrorMessage(field: string): string {
+    switch (field) {
+      case 'participant':
+        if (this.sessionForm.get('participant').hasError('required')) {
+          return 'Bitte gib deinen Namen ein';
+        } else if (this.sessionForm.get('participant').hasError('minlength')) {
+          return 'Gib bitte mindestens drei Zeichen ein';
+        } else if (this.sessionForm.get('participant').hasError('maxlength')) {
+          return 'Bitte beschränke dich auf 16 Zeichen';
+        }
+      case 'session':
+        return 'Bitte wähle eine Session aus';
+      case 'team':
+        return 'Bitte wähle ein Team aus';
+    }
+    return 'Unbekannter Fehler';
+  }
+
+  onSubmit() {
+    console.log(this.sessionForm.value); // { first: '', last: '' }
+    console.log(this.sessionForm.valid); // false
   }
 
   getSessions(): void {
@@ -56,15 +135,10 @@ export class JoinSessionComponent implements OnInit {
       });
   }
 
-  selectSession2Join(joinSession: JoinSession): void {
-    this.selectedSession2Join = joinSession;
-  }
+  selectSession2Join(joinSession: JoinSession): void {}
 
   updateTeamMembers(): void {
-    const selectedTeam = this.selectedSession2Join.teams.find(
-      (team: Team) => team.name === this.selectedTeam2Join
-    );
-
+    const selectedTeam = this.sessionForm.get('team').value;
     if (selectedTeam.players.length === 0) {
       this.teamMembers = ' ist noch niemand';
     } else if (selectedTeam.players.length === 1) {
@@ -91,35 +165,28 @@ export class JoinSessionComponent implements OnInit {
   }
 
   joinSession(): void {
-    if (this.selectedSession2Join === null) return;
-
-    const body = new URLSearchParams();
-    body.set('participant', this.participant);
-    body.set('session', this.selectedSession2Join.name);
-    body.set('team', this.selectedTeam2Join);
-
-    this.http
-      .post(
-        `/api/join-session`,
-        body.toString(),
-        environment.formHeader
-      )
-      .toPromise()
-      .then((response: Response<Session>) => {
-        this.activeSession = response.data;
-        console.info('Active Session:', this.activeSession);
-
-        this.userService.setUser({
-          session: this.activeSession.name,
-          team: this.selectedTeam2Join,
-          player: this.participant,
-        });
-        this.user = this.userService.getUser();
-
-        this.router.navigateByUrl('/spielen'); // TODO: navigate to correct game (use ID or session name as URL parameter)
-      })
-      .catch((error: any) => {
-        console.error('Could not join session:', error);
-      });
+    // TODO: refactor
+    // if (this.selectedSession2Join === null) return;
+    // const body = new URLSearchParams();
+    // body.set('participant', this.participant);
+    // body.set('session', this.selectedSession2Join.name);
+    // body.set('team', this.selectedTeam2Join);
+    // this.http
+    //   .post(`/api/join-session`, body.toString(), environment.formHeader)
+    //   .toPromise()
+    //   .then((response: Response<Session>) => {
+    //     this.activeSession = response.data;
+    //     console.info('Active Session:', this.activeSession);
+    //     this.userService.setUser({
+    //       session: this.activeSession.name,
+    //       team: this.selectedTeam2Join,
+    //       player: this.participant,
+    //     });
+    //     this.user = this.userService.getUser();
+    //     this.router.navigateByUrl('/spielen'); // TODO: navigate to correct game (use ID or session name as URL parameter)
+    //   })
+    //   .catch((error: any) => {
+    //     console.error('Could not join session:', error);
+    //   });
   }
 }
--- apps/alias-game-frontend/src/app/join-session/join-session.module.ts
+++ apps/alias-game-frontend/src/app/join-session/join-session.module.ts
@@ -3,11 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { JoinSessionRoutingModule } from './join-session-routing.module';
 import { JoinSessionComponent } from './join-session.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [JoinSessionComponent],
-  imports: [CommonModule, JoinSessionRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    JoinSessionRoutingModule,
+    FormsModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
   exports: [JoinSessionComponent],
 })
 export class JoinSessionModule {}
